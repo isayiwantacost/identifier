1ee6e45a3dd4df105e0fabd729fda86ddaf6283b
Saloom <116690661+isayiwantacost@users.noreply.github.com>
2023-03-12 00:19:18 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const langNames = {
+    h5: 'html',
+    ts: 'typescript',
+    l: 'lua',
+    c: 'css',
+    js: 'javascript'
+}
+
+function IdentifyLangs(langNames, h5) {
+    const LangH = langNames.html(h5[i])
+    const LangT = langNames.typescript(ts[i])
+    const LangL = langNames.lua(l[i])
+    const LangC = langNames.css(c[i])
+    const LangJ = langNames.javascript(js[i])
+    const LangALL = "html, typescript, lua, css, javascript"
+}
+
+function FindLang(langNames, IdentifyLangs) {
+    const index = IdentifyLangs.LangALL
+    
+    return `
+    .langNames(
+        (langNames, index) =>
+        {i, index}
+        )
+        `;
+        
+}
+
+function printLang(langNames, index)
+{
+    print(langNames, index)
+}
